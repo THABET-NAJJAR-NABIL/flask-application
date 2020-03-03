f82569ee1c9594490d26475bd0ea3eb2fa66daac
THABET-NAJJAR Nabil <thabetnajjarnabil@gmail.com>
2020-03-03 05:45:13 +0100

    Flask + SQLQlchemy CRUD

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
+import psycopg2
 
 # init app
 app = Flask(__name__)
@@ -17,8 +18,8 @@ ma = Marshmallow(app)
 
 
 # Product Class/ Model
-class Prodcut(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True, )
     name = db.Column(db.String(100), unique=True)
     description = db.Column(db.String(200))
     price = db.Column(db.Float)
@@ -50,22 +51,56 @@ def add_product():
     description = request.json['description']
     price = request.json['price']
     quantity = request.json['quantity']
-    new_product = Prodcut(name, description, price, quantity)
-    # db.session.add(new_product)
-    # db.session.commit()
+    new_product = Product(name=name, description=description, price=price, quantity=quantity)
+    db.session.add(new_product)
+    db.session.commit()
     print(new_product)
     return product_schema.jsonify(new_product)
 
 
+# update a  product
+@app.route('/updateProduct/<id>', methods=['PUT'], )
+def update_product(id):
+    product = Product.query.get(id)
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    quantity = request.json['quantity']
+    product.name = name
+    product.description = description
+    product.price = price
+    product.quantity = quantity
+    db.session.commit()
+    return product_schema.jsonify(product)
+
+
+# Delete a product by id
+@app.route("/product/<id>", methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+    return jsonify(product_schema.dump(product))
+
+# Get all products
+@app.route("/products", methods=['GET'])
+def get_all_products():
+    all_products = Product.query.all()
+    result = products_schema.dump(all_products)
+    return jsonify(result)
+
+# Get a product by id
+@app.route("/product/<id>", methods=['GET'])
+def get_one_product(id):
+    product = Product.query.get(id)
+    return jsonify(product_schema.dump(product))
+
+
 # create a product
-@app.route('/product', methods=['POST'], )
+@app.route('/product', methods=['POST'])
 def get_product():
-    data = request.get_data()
-    print(data)
-
-    name = request.args['name']
-
-    return name, 200
+    data = request.json['name']
+    return data, 200
 
 
 # Run Server
@@ -74,5 +109,5 @@ def get():
     return jsonify({"msg": "Hello World!"})
 
 
-if (__name__ == "__main__"):
-    app.run(port=5000)
+if __name__ == "__main__":
+    app.run(debug=False, port=5000)
